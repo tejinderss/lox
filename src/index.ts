0c51b5d5dee061fe3e42ece567b8bac82effa3be
Tejinder Singh <tejinderss@gmail.com>
2025-01-14 18:17:27 +0000

    Adding command line parsing

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,17 @@
-export function main(name?: string): string {
-    return `Hello, ${name ?? 'World'}!`;
+import { parseArgs } from 'node:util';
+import { run_prompt, run_file } from './lox.ts';
+
+export function main(): void {
+    const { positionals } = parseArgs({
+        allowPositionals: true,
+    });
+    if (positionals.length > 1) {
+        throw new Error("Usage: lox [script]");
+    } else if (positionals.length == 1) {
+        run_file(positionals[0]);
+    } else {
+        run_prompt();
+    }
 }
 
-console.log(main());
+main();
---
+++ src/lox.ts
@@ -0,0 +1,7 @@
+export function run_file(path: string) {
+    throw new Error(`Not Implemented yet ${path}`);
+}
+
+export function run_prompt(): void {
+    throw new Error('Not implemented yet');
+}
